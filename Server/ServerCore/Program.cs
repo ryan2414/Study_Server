157fe7f90939d7b7ead9f9ddbcc37955a9416fa5
ryan2414 <jackpar@nate.com>
2023-11-26 22:07:10 +0900

    캐시이론

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -5,35 +5,31 @@ namespace ServerCore
 {
     internal class Program
     {
-        volatile static bool _stop = false; // 코드 최적화를 하지 말아라. 사용 권장 X
-        static void ThreadMain()
-        {
-            Console.WriteLine("쓰레드 시작!");
-
-            while (_stop == false)
-            {
-                // 누군가가 stop 신호를 해주기를 기다린다.
-            }
-
-            Console.WriteLine("쓰레드 종료!");
-
-        }
-
         static void Main(string[] args)
         {
-            Task t = new Task(ThreadMain);
-            t.Start();
+            // 5 * 5 배경
+            // [][][][][] [][][][][] [][][][][] [][][][][] [][][][][]
+            int[,] arr = new int[10000, 10000];
 
-            Thread.Sleep(1000);
-
-            _stop = true;
-
-            Console.WriteLine("Stop 호출!");
-            Console.WriteLine("종료 대기중");
-            
-            t.Wait();
-            
-            Console.WriteLine("종료 성공");
+            // Spacial Locality로 상대적으로 빠름.
+            // 주변의 정보를 캐시해 놓음
+            {
+                long now = DateTime.Now.Ticks;
+                for (int i = 0; i < 10000; i++)
+                    for (int j = 0; j < 10000; j++)
+                        arr[i, j] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y, x) 순서 걸린 시간 {end - now}");
+            }
+            // 캐시를 사용할 수 없는 코드 
+            {
+                long now = DateTime.Now.Ticks;
+                for (int i = 0; i < 10000; i++)
+                    for (int j = 0; j < 10000; j++)
+                        arr[j, i] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(x, y) 순서 걸린 시간 {end - now}");
+            }
         }
     }
 }
--- Server/ServerCore/obj/Debug/net8.0/ServerCore.AssemblyInfo.cs
+++ Server/ServerCore/obj/Debug/net8.0/ServerCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20146c9c79d12310817d3be067f0a2ee98e6fe1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
