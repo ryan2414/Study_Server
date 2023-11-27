bd670f748256cc50e567e0691e2704a29e836ad8
ryan2414 <jackpar@nate.com>
2023-11-27 21:48:20 +0900

    메모리 배리어 #2

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ServerCore
+﻿using System.Buffers;
+
+namespace ServerCore
 {
     // 메모리 배리어
     // A) 코드 재배치 억제
@@ -8,55 +10,32 @@
     // 2) Store Memory Barrier (ASM SFENCE) : Store만 막는다.
     // 3) Load Memory Barrier (ASM LFENCE) : Load만 막는다.
 
-    internal class Program
+    class Program
     {
-        static volatile int x = 0;
-        static volatile int y = 0;
-        static volatile int r1 = 0;
-        static volatile int r2 = 0;
+        int _answer;
+        bool _complete;
 
-        // 하드웨어가 지 마음데로 순서를 연관된 것이 없으면 순서를 바꿀 수도 있음.
-        
-        static void Thread_1()
+        void A()
         {
-            y = 1; // Store y
-
-            // -------------------------------
-            Thread.MemoryBarrier();
-            
-            r1 = x; // Load x
+            _answer = 123;
+            Thread.MemoryBarrier(); // Barrier 1
+            _complete = true;
+            Thread.MemoryBarrier(); // Barrier 2
         }
 
-        static void Thread_2()
+        void B()
         {
-            x = 1; // Story x
-
-            // -------------------------------
-            Thread.MemoryBarrier();
-
-            r2 = y; // Load y
+            Thread.MemoryBarrier(); // Barrier 3
+            if ( _complete )
+            {
+                Thread.MemoryBarrier(); // Barrier 4
+                Console.WriteLine(_answer);
+            }
         }
 
         static void Main(string[] args)
         {
-            int count = 0;
-            while (true)
-            {
-                count++;
-                x = y = r1 = r2 = 0;
-
-                Task t1 = new Task(Thread_1);
-                Task t2 = new Task(Thread_2);
-                t1.Start();
-                t2.Start();
-
-                Task.WaitAll(t1, t2);
-
-                if (r1 == 0 && r2 == 0)
-                    break;
-            }
-
-            Console.WriteLine($"{count} 번 만에 빠져나옴!");
+            
         }
     }
 }
