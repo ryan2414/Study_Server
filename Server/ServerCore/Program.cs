ae859ac2195d58a2a7ccc37f7368f27beae2cc3d
ryan2414 <jackpar@nate.com>
2023-11-28 22:21:19 +0900

    Spin Lock

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,109 +1,66 @@
 ﻿namespace ServerCore
 {
-    class FastLock
+    class SpinLock
     {
-        public int id;
-    }
+        volatile int _locked = 0;
 
-    internal class SessionManager
-    {
-        private FastLock l;
-        private static object _lock = new object();
-
-        public static void TestSession()
+        public void Acquire()
         {
-            lock (_lock)
+            while (true)
             {
+                //int original = Interlocked.Exchange(ref _locked, 1);
+                //if (original == 0) // 내가 처음 들어갔다!
+                //    break;
+
+                //
+                // CAS Compare-And-Swap
+                //
+
+                //if (_locked == 0)
+                //    _locked = 1;
+                int expected = 0;
+                int desired = 1;
+                if (Interlocked.CompareExchange(ref _locked, desired, expected) == expected) 
+                    break;
+                
+                //{
+                //    int original = _locked;
+                //    _locked = 1;
+                //    if (original == 0)
+                //        break;
+                //}
             }
+           
         }
 
-        public static void Test()
+        public void Release()
         {
-            lock (_lock)
-            {
-                UserManager.TestUser();
-            }
-        }
-    }
-
-    internal class UserManager
-    {
-        private FastLock l;
-        private static object _lock = new Object();
-
-        public static void Test()
-        {
-            //Monitor.TryEnter()
-            lock (_lock)
-            {
-                SessionManager.TestSession();
-            }
-        }
-
-        public static void TestUser()
-        {
-            lock (_lock)
-            {
-            }
+            _locked = 0;
         }
     }
 
     internal class Program
     {
-        private static int number = 0;
-        private static object _obj = new object();
-        private static object _obj2 = new object();
+        private static int _num = 0;
+        static SpinLock _lock = new SpinLock();
 
-        private static void Thread_1()
+        static void Thread_1()
         {
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 100000; i++)
             {
-                SessionManager.Test();
-
-                // 상호배제 Mutual Exclusive
-                //lock (_obj)
-                //{
-                //    number++;
-                //}
-
-                //try
-                //{
-                //    Monitor.Enter(_obj);
-                //    number++;
-
-                //    return;
-                //}
-                //finally
-                //{
-                //    Monitor.Exit(_obj);
-                //}
-
-                // CriticalSection std:mutex
-                //Monitor.Enter(_obj); // 문을 잠그는 행위
-                //{
-                //    number++;
-
-                //}
-                //Monitor.Exit(_obj); // 잠금을 풀어준다.
+                _lock.Acquire();
+                _num++;
+                _lock.Release();
             }
         }
 
-        // 데드락 DeadLock
-
-        private static void Thread_2()
+        static void Thread_2()
         {
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 100000; i++)
             {
-                UserManager.Test();
-                //lock (_obj)
-                //{
-                //    number--;
-                //}
-                //Monitor.Enter(_obj);
-
-                //number--;
-
-                //Monitor.Exit(_obj);
+                _lock.Acquire();
+                _num--;
+                _lock.Release();
             }
         }
 
@@ -112,14 +69,11 @@
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
             t1.Start();
-
-            Thread.Sleep(100);
-
             t2.Start();
 
             Task.WaitAll(t1, t2);
 
-            Console.WriteLine(number);
+            Console.WriteLine(_num);
         }
     }
 }
